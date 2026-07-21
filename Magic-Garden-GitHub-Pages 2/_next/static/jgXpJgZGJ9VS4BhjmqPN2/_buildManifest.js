0aa6e58b02fba53ca18b3a0a31c777fac190b495
suh39478-lgtm <suh39478@gmail.com>
2026-07-21 16:14:42 +0800

    Add files via upload

---
+++ Magic-Garden-GitHub-Pages 2/_next/static/jgXpJgZGJ9VS4BhjmqPN2/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/magic-garden//_next/:path+",
+        "destination": "/magic-garden/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
